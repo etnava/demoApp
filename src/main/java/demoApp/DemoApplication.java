d9e5b1369cbe4ed9632e4246831d52baa9ccc992
Edward S <38728269+etnava@users.noreply.github.com>
2020-05-20 16:08:35 +1000

    Added further comments

--- src/main/java/demoApp/DemoApplication.java
+++ src/main/java/demoApp/DemoApplication.java
@@ -19,11 +19,25 @@ public class DemoApplication {
 	
 	/*
 	 * Create the model classes
-	 * Create the repository
+	 * Ensure @Component & @Entity and @Id annotations
+	 * Create the repository interfaces
+	 * Ensure @Component annotation
 	 * Create setup dataloader (preload the data here)
+	 * Ensure @Component Annotation and that dataloader extends ApplicationRunner 
+	 *		 and implements the method run()
+	 *		@Autowired the repositories required
 	 * Create the Controller
+	 * 		@RestController Annotation
+	 * 		@GetMapping
+	 * 		@RequestMapping on the class is optional
 	 * Create the service class to handle conversion from city to city DTO
+	 * 		Handles conversion for City to CITYDTO, does not save in repository here.
 	 * 
+	 * To access h2 database
+	 * http://localhost:5000/h2-console
+	 * 
+	 * jdbcURL =  jdbc:h2:mem:testdb
+	 * user name = sa
 	 */
 	
 }
